fd1e9898be032fe51af7f77b6b897f3b2b4e6fa9
Gawshan987 <gawshan.sri@gmail.com>
2023-08-01 13:14:28 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Popup from "./Popup";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Popup />
+    </div>
+  );
+}
---
+++ src/Popup.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Popup() {
+  return (
+    <div>
+      <h1>Frogger</h1>
+      <p>Welcome to Frogger, the objective of the game is to </p>
+    </div>
+  );
+}
