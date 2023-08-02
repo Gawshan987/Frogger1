56c985c4c339cf36a27224dd9696227bef9bd94d
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2023-08-02 11:29:32 -0400

    Added tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Popup from "./Popup";
+import GameBoard from "./GameBoard";
 
 export default function App() {
   return (
     <div className="App">
       <Popup />
+      <GameBoard />
     </div>
   );
 }
