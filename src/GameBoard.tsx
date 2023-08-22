068a12c9ba04e7ccab3b88370c1663e4aac65c7b
Gawshan987 <gawshan.sri@gmail.com>
2023-08-22 12:56:19 -0400

    Fixed gird

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -111,7 +111,7 @@ const GameBoard = () => {
           <div className="text-4xl text-center mt-4">
             Welcome to Frogger!
             <div className="text-center mt-4">
-              <div className="grid grid-cols-2 gap-4 items-center">
+              <div className="grid grid-cols-1 gap-4 items-center">
                 <h1 className="text-xl">
                   The objective of this game is to cross to the other side
                   without crashing into the other cars moving. To play this game
